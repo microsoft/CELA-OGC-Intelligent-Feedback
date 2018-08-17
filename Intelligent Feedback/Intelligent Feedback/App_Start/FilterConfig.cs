fbf480a0826ad95ed333fd92c41ebf8adbc2ae36
Jason Barnwell (CELA) <jabarnwe@microsoft.com>
2018-08-16 23:09:35 -0700

    Adding Power BI embed and Application Insights telemetry.

--- Intelligent Feedback/Intelligent Feedback/App_Start/FilterConfig.cs
+++ Intelligent Feedback/Intelligent Feedback/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Intelligent_Feedback
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
