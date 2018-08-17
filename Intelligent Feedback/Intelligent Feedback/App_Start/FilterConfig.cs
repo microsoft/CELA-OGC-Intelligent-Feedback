2005c998db789548a3e26cc2317d63484aa7cde1
Jason Barnwell (CELA) <jabarnwe@microsoft.com>
2018-08-16 22:34:59 -0700

    Initial commit of shell MVC site.

---
+++ Intelligent Feedback/Intelligent Feedback/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Intelligent_Feedback
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
